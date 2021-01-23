521fcd996f9dc8b6055af74812b295a6d3a2ccdf
kasambhuvan2006 <kasambhuvan@gmail.com>
2021-01-23 14:09:52 +0530

    Third Commit

--- Auxal App.py
+++ Auxal App.py
@@ -14,11 +14,7 @@ conn = sqlite3.connect("Signup_details.db")
 
 c = conn.cursor()
 
-'''c.execute(""" CREATE TABLE Info(
-Name text,
-Username text,
-Password text,
-Email text )""")'''
+
 conn.commit()
 
 
@@ -57,9 +53,75 @@ logind_image = ImageTk.PhotoImage(Image.open("3.png"))
 logind_btn = Button(root, image = logind_image)
 logind_btn.place(x = 1200, y= 700)
 
+def SignDoctor():
+      U_E.destroy()
+      U_E_Label.destroy()
+      P_E.destroy()
+      P_E_Label.destroy()
+      login_btn.destroy()
+      logind_btn.destroy()
+      signupd_btn.destroy()
+      signup_btn.destroy()
+
+      #Sign up page for doctors
+
+
+
+      root = Tk()
+      root.title("Doctor Sign-Up")
+
+      fullname = Entry(root,  width = 100)
+      fullname.place(x = 350, y = 260)
+      label1 = Label(root, text = "Full name: ").place(x = 320, y = 260)
+
+      email = Entry(root, width = 100)
+      email.place(x = 350, y = 310)
+      label2 = Label(root, text = "Email address: ").place(x = 320, y = 310)
+
+      username = Entry(root, width = 100)
+      username.place(x = 350, y = 360)
+      label3 = Label(root, text = "Username: ").place(x = 320, y = 360)
+
+      password = Entry(root, width = 100)
+      password.place(x = 350, y = 410)
+      label4 = Label(root, text = "Password: ").place(x = 320, y = 410)
+
+      confirm = Entry(root, width = 100)
+      confirm.place(x = 350, y = 460)
+      label5 = Label(root, text = "Confirm Password: ").place(x = 320, y = 460)
+
+      linkedin = Entry(root, width = 100)
+      linkedin.place(x = 350, y = 510)
+      label6 = Label(root, text = "Linkedin Profile Link: ").place(x = 320, y = 510)
+
+      specialization = Entry(root, width = 100)
+      specialization.place(x = 350, y = 560)
+      label7 = Label(root, text = "Field of Specialization: ").place(x = 320, y = 560)
+
+      label8 = Label(root, text = "Proof of Certification: ").place(x = 320, y = 610)
+
+
+
+
 signupd_image = ImageTk.PhotoImage(Image.open("4.png"))
-signupd_btn = Button(root, image = signupd_image)
+signupd_btn = Button(root, image = signupd_image, commad = SignDoctor)
 signupd_btn.place(x = 100, y= 700)
+
+##def open():
+##    #global my_image
+##    #root.filename = filedialog.askopenfilename(initialdir = "C:", title = "Select a File", filetypes = (("PNG files","*.png"), ("all files", "*.*")))
+##
+##
+##
+##    my_image =Image.open("C:\\Users\\kasam\\Desktop\\x and y .png")
+##    resized = my_image.resize((1000,1000))
+##    new_pic = ImageTk.PhotoImage(resized)
+##    image_label = Label(image = new_pic)
+##    image_label.place(x = 800, y = 800)
+##    print("read")
+
+#open_certificate = Button(root, text = "Open File", command = open).place(x = 450, y = 610)
+
 
 def register():
       U_E.destroy()
@@ -101,6 +163,7 @@ def register():
 
 
       def Register():
+            
         #Send email and error
         regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
 
@@ -123,12 +186,14 @@ def register():
 
 
         def check(em):
-            if(re.search(regex,em)):  
-                print("Valid Email")  
-                  
-            else:  
-                print("Invalid Email")
-                messagebox.showwarning("Warning", "Invalid Email")
+              
+              
+              if(re.search(regex,em)):
+                    print("Valid Email")  
+                        
+              else:
+                    print("Invalid Email")
+                    messagebox.showwarning("Warning", "Invalid Email")
 
           
 
@@ -148,6 +213,9 @@ def register():
         # create cursor
 
         c = conn.cursor()
+      
+        conn.commit()
+        
 
         #insert into tabel
         c.execute("INSERT INTO Info VALUES (:Name, :Username, :Password, :Email )",
@@ -159,6 +227,11 @@ def register():
 
 
                         })
+
+        Name.delete(0,END)
+        Password.delete(0,END)
+        Email.delete(0,END)
+        Username.delete(0,END)
       signup_image1 = ImageTk.PhotoImage(Image.open("2.png"))
       signup_btn2 = Button(root, image = signup_image, command = Register)
       signup_btn2.place(x = 700, y= 700)
